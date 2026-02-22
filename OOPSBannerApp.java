ffb793f2ae527583848abae806f05a1f564c00dd
girish-69 <gc7513@srmist.edu.in>
2026-02-23 03:08:07 +0530

    UC4: Print OOPS banner using array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,25 +2,18 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join("",
-                " *****  ", " *****  ", " *****  ", " ***** "));
+        String[] banner = {
+            String.join("", " *****  ", " *****  ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*     * ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*       ", "      *"),
+            String.join("", "*     * ", "*     * ", "*       ", "      *"),
+            String.join("", " *****  ", " *****  ", "*       ", " ***** ")
+        };
 
-        System.out.println(String.join("",
-                "*     * ", "*     * ", "*     * ", "*      "));
-
-        System.out.println(String.join("",
-                "*     * ", "*     * ", "*     * ", "*      "));
-
-        System.out.println(String.join("",
-                "*     * ", "*     * ", " *****  ", " ***** "));
-
-        System.out.println(String.join("",
-                "*     * ", "*     * ", "*       ", "      *"));
-
-        System.out.println(String.join("",
-                "*     * ", "*     * ", "*       ", "      *"));
-
-        System.out.println(String.join("",
-                " *****  ", " *****  ", "*       ", " ***** "));
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
